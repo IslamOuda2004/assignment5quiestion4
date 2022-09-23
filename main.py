114621523adcbed302ba87baa37f242379b058fb
IslamOuda2004 <113916264+IslamOuda2004@users.noreply.github.com>
2022-09-23 21:07:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+keys = ['ten','twenty','thrty']
+val = [10,20,30]
+count = 0
+dict = {}
+
+while count < len(keys):
+    dict[keys[count]] = val[count]
+    count += 1
+
+print(dict)
+
+#contact me if the homework requires to make it taking inputs from user
+
+
